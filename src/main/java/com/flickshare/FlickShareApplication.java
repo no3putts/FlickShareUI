73f9090ec7eae1fe2492a3da266f5dc32e7ca45a
Jerrie Pineda <jeremiah.pineda@stevenshenager.edu>
2020-11-14 21:55:53 -0700

    Initial Commit

---
+++ src/main/java/com/flickshare/FlickShareApplication.java
@@ -0,0 +1,13 @@
+package com.flickshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlickShareApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into inventory_item(id, name, description, price, image_url) values (RANDOM_UUID(),'The Vase','Hitlers Vase',76000.00, 'https://www.pexels.com/photo/flowerful-flowery-flowerly-42257');
+insert into inventory_item(id, name, description, price) values (RANDOM_UUID(),'Grandpa Clock','Einsteins Clock',6000.00);
+insert into inventory_item(id, name, description, price) values (RANDOM_UUID(),'Guitar','Van Halen',400.00);
+insert into inventory_item(id, name, description, price) values (RANDOM_UUID(),'Ball','Jordans Ball',100.00);
+insert into inventory_item(id, name, description, price) values (RANDOM_UUID(),'Banjo','Hitlers Banjo',1200.00);
+insert into inventory_item(id, name, description, price) values (RANDOM_UUID(),'Bike','Evils Bike',50.00);
